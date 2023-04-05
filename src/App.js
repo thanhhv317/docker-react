5991f7ea014a8f2cd8585d7093fae02b3457b4b7
thanhhv317 <thanhhoang317@gmail.com>
2023-04-05 09:57:58 +0700

    Changed app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there!
+          I was chaned on the Feature branch
         </p>
         <a
           className="App-link"
